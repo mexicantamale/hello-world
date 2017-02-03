d111f702a205c6e1b3a604ebd06f3e1b8e5ad165
mexicantamale <panabanana365@gmail.com>
2017-02-03 15:15:00 -0800

    Update new.java

--- new.java
+++ new.java
@@ -1,25 +1,47 @@
-var $cont = document.querySelector('.cont');
-var $elsArr = [].slice.call(document.querySelectorAll('.el'));
-var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+$(function() {
 
-setTimeout(function() {
-  $cont.classList.remove('s--inactive');
-}, 200);
-
-$elsArr.forEach(function($el) {
-  $el.addEventListener('click', function() {
-    if (this.classList.contains('s--active')) return;
-    $cont.classList.add('s--el-active');
-    this.classList.add('s--active');
-  });
+//Image by Ivaylo Yovchev (  http://ivayloyovchev.com/weddings )
+  
+	//elements
+	var $menu = $(".menu");
+	var $pages = $(".page");
+	var $menuLi = $menu.find("li").not(".to-home");
+	var $toHome = $menu.find(".to-home");
+	
+	//interna vars
+	var currentPage = "";
+	
+	$toHome.on("click", function() {
+		currentPage = "";
+		TweenMax.to($pages, 0.5, {
+			left: "-70%"
+		});
+		TweenLite.to($menuLi.filter(".active"), 0.5, {
+			className: "-=active"
+		});
+	});
+	
+	$menuLi.on("click", function(event) {
+		
+		var $this = $(this);
+		var thisHref = $this.find("a").attr("href");
+		
+		if (currentPage !== thisHref && $pages.filter(thisHref).length > 0) {
+			currentPage = thisHref;
+			TweenMax.to($pages, 0.5, {
+				left: "-70%"
+			});
+			TweenMax.to($pages.filter(thisHref), 0.5, {
+				left: 0
+			});
+			TweenLite.to($menuLi.filter(".active"), 0.5, {
+				className: "-=active"
+			});
+			TweenLite.to($this, 0.5, {
+				className: "+=active"
+			});
+		}
+		
+		event.preventDefault();
+	});
 });
-
-$closeBtnsArr.forEach(function($btn) {
-  $btn.addEventListener('click', function(e) {
-    e.stopPropagation();
-    $cont.classList.remove('s--el-active');
-    document.querySelector('.el.s--active').classList.remove('s--active');
-  });
-});
-
-
